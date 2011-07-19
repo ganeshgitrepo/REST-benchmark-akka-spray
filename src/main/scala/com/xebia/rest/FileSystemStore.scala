1402ea5ad3f7f79a51473dae68ce0425671b41b7
Andrew Snare <asnare@xebia.com>
2011-07-19 20:17:30 +0200

    Use our own actor; this lets us access the uuid and ensure concurrent writes don't collide.

--- src/main/scala/com/xebia/rest/FileSystemStore.scala
+++ src/main/scala/com/xebia/rest/FileSystemStore.scala
@@ -1,38 +1,53 @@
 package com.xebia.rest
 
-import akka.actor.Actor
 import akka.actor.Actor._
 import cc.spray.json._
 import RecordJsonProtocol._
 import akka.dispatch.Future
 import io.Source
 import java.io.{FileNotFoundException, FileOutputStream, File}
+import com.xebia.rest.RecordStoreMessages._
+import akka.actor.{PoisonPill, Actor}
+import org.omg.IOP.Codec
 
 object FileSystemStore extends RecordStore {
-  val fsRoot = new File("/tmp/spray")
-
   def get(key: Long) = {
-    Future[Option[Record]] {
-      val recordLocation = new File(fsRoot, key + "")
-      try {
-        val rawRecord = Source.fromFile(recordLocation, "UTF-8").getLines().mkString
-        Some(JsonParser(rawRecord).fromJson[Record])
-      } catch {
-        case e:FileNotFoundException => None
-      }
-    }
+    // Spawn an actor just for this task.
+    val newActor = actorOf[FileSystemStore].start()
+    val futureResult = newActor !!! Get(key)
+    newActor ! PoisonPill
+    futureResult
   }
 
   def put(key: Long, value: Record) = {
-    spawn {
-      val pendingRecordLocation = new File(fsRoot, key + ".tmp")
+    // Spawn an actor just for this task.
+    val newActor = actorOf[FileSystemStore].start()
+    newActor ! Put(key, value)
+    newActor ! PoisonPill
+  }
+}
+
+class FileSystemStore extends Actor {
+  val fsRoot = new File("/tmp/spray")
+  def receive = {
+    case Get(id) => {
+      val recordLocation = new File(fsRoot, id.toString)
+      try {
+        val rawRecord = Source.fromFile(recordLocation, "UTF-8").getLines().mkString
+        self.channel ! Some(JsonParser(rawRecord).fromJson[Record])
+      } catch {
+        case e:FileNotFoundException => self.channel ! None
+      }
+    }
+    case Put(id, value) => {
+      val pendingRecordLocation = new File(fsRoot, id + '.' + self.uuid.toString)
       val recordOutputStream = new FileOutputStream(pendingRecordLocation)
       try {
         recordOutputStream.write(value.toJson.toString.getBytes("UTF-8"))
       } finally {
         recordOutputStream.close()
       }
-      val finalRecordLocation = new File(fsRoot, key + "")
+      val finalRecordLocation = new File(fsRoot, id.toString)
       pendingRecordLocation.renameTo(finalRecordLocation)
     }
   }
